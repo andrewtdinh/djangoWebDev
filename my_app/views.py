084b3b49224b2cf19102c682df134e483534f228
Andrew Dinh <andrewdinh74@yahoo.com>
2025-10-02 14:58:40 -0700

    Create url pattern to match the home route

--- my_app/views.py
+++ my_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-def index():
-  return HttpResponse('Hello World!')
+def index(request):
+  return HttpResponse('You are being indexed!')

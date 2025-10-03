e2497f2b773d9a3d6f2c76e2c6b0bea91cf8ed76
Andrew Dinh <andrewdinh74@yahoo.com>
2025-10-02 18:27:21 -0700

    Add an about page

--- my_app/views.py
+++ my_app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
   return HttpResponse('You are being indexed!')
+
+def about(request):
+  return HttpResponse('My name is Andrew Dinh and I am playing with Django!')

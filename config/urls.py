0c011c59c436fd5e646e555618142ab499e0a0c4
Andrew Dinh <andrewdinh74@yahoo.com>
2025-10-03 11:08:36 -0700

    Create dynamic hello <firstname> route

--- config/urls.py
+++ config/urls.py
@@ -17,10 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from my_app.views import index, about
+from my_app.views import index, about, hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
     path('about/', about),
+    path('hello/<str:first_name>', hello),
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -6,3 +6,6 @@ def index(request):
 
 def about(request):
   return HttpResponse('My name is Andrew Dinh and I am playing with Django!')
+
+def hello(request, first_name):
+  return HttpResponse(f"Hello {first_name}")
